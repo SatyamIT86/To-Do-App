8f1b596d3502fc0f0953e1fcf16180e09e1dabc1
Demo <pandeysatyam9833@gmail.com>
2025-07-09 23:01:59 +0530

    adding Todo Tile

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/todo_tile.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -17,7 +18,82 @@ class _HomepageState extends State<Homepage> {
         title: Text("Todo App"),
         elevation: 0,
       ),
-      body: ListView(),
+      body: ListView(
+        children: [
+          TodoTile(
+            taskName: "Do Code",
+            taskCompleted: true,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Activities",
+            taskCompleted: false,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do marketing",
+            taskCompleted: false,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Flutter",
+            taskCompleted: true,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Learn Something new",
+            taskCompleted: true,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Learn Something new",
+            taskCompleted: false,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Learn Something new",
+            taskCompleted: true,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+          TodoTile(
+            taskName: "Do Learn Something new",
+            taskCompleted: true,
+            onChanged: (value) {
+              setState(() {
+                // Handle checkbox state change
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
